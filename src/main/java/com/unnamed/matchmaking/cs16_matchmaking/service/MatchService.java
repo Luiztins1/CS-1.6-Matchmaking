6fe4b51c2b14567e9281596a7b803e5fea9762b9
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-03-28 23:13:33 -0300

    feat: service Match created.

---
+++ src/main/java/com/unnamed/matchmaking/cs16_matchmaking/service/MatchService.java
@@ -0,0 +1,47 @@
+package com.unnamed.matchmaking.cs16_matchmaking.service;
+
+import com.unnamed.matchmaking.cs16_matchmaking.model.Match;
+import com.unnamed.matchmaking.cs16_matchmaking.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class MatchService {
+
+    private final MatchRepository matchRepository;
+
+    @Autowired
+    public MatchService(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
+    public Match saveMatch(Match match){
+        return matchRepository.save(match);
+    }
+
+    public List<Match> findAllMatch(){
+        return matchRepository.findAll();
+    }
+
+    public Optional<Match> updateMatch(UUID uuid, Match match){
+        return matchRepository.findById(uuid).map(existMatch ->{
+            existMatch.setMap(match.getMap());
+            existMatch.setTimeMatchMap(match.getTimeMatchMap());
+            existMatch.setListPlayer(match.getListPlayer());
+            return matchRepository.save(existMatch);
+        });
+    }
+
+    public void deleteMatch(UUID uuid){
+        matchRepository.deleteById(uuid);
+    }
+
+    public Optional<Match> findByIdMatch(UUID uuid){
+        return matchRepository.findById(uuid);
+    }
+
+}
