8cd922cea4ed7af37789e21f74feeb65f82cf3db
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-03-28 22:09:22 -0300

    init.

---
+++ src/main/java/com/unnamed/matchmaking/cs16_matchmaking/Cs16MatchmakingApplication.java
@@ -0,0 +1,13 @@
+package com.unnamed.matchmaking.cs16_matchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs16MatchmakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs16MatchmakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unnamed/matchmaking/cs16_matchmaking/Cs16MatchmakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unnamed.matchmaking.cs16_matchmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs16MatchmakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
